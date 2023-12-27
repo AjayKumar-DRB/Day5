abe19f202b57351a47395cd979711478758ea6cb
Ajay Kumar <96405422+AjayKumar-DRB@users.noreply.github.com>
2023-12-27 16:21:00 +0530

    Add files via upload

---
+++ Day5-Exercise2.js
@@ -0,0 +1,41 @@
+let myResume = {
+    "name": "Ajay Kumar D R B",
+    "title": "Software Engineer",
+    "contact": {
+        "phone": "7904008981",
+        "linkedin": "linkedin.com/in/ajay-d-r-b-kumar/",
+        "email": "ajaykumardrb@gmail.com",
+        "github": "github.com/AjayKumar-DRB"
+    },
+    "education": [
+      {
+        "degree": "Bachelor of Engineering",
+        "specialization": "Mechanical Engineering",
+        "school": "K.L.N. College of Enginnering",
+        "year": 2022
+      }
+    ],
+    "experience": [
+      {
+        "position": "Subject Matter Expert - Mathematics",
+        "company": "Chegg India Pvt. Ltd.",
+        "location": "Remote, India",
+        "start_date": "01-02-2021",
+        "end_date": "Present",
+        "responsibilities":[
+          "Developing and reviewing mathematical content for educational materials.",
+          "Providing expertise in creating curriculum aligned with educational standards.",
+          "Offering guidance to instructional designers and educators on effective teaching methodologies in mathematics."
+        ]
+      }
+    ],
+    "skills": [
+      {
+        "frontend": ["HTML", "CSS", "JavaScript"],
+        "backend": ["Java", "Python"],
+        "database": ["MySQL", "MS SQL Server"]
+      }
+    ],
+    "languages": ["Sourashtra", "English", "Tamil", "Hindi"],
+    "interests": ["Video Games", "Webnovels", "Swimming", "Open-Source Projects"]
+  };
